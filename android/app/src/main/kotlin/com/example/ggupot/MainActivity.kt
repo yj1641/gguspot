1cb02ae73852ac5b39012227372c05c68c04ad61
YJ <killingtime7@naver.com>
2022-05-02 16:47:05 +0900

    naver map 적용

--- android/app/src/main/kotlin/com/example/ggupot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ggupot/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ggupot
+package com.jin.ggupot
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/screens/map/map_screen.dart
+++ lib/screens/map/map_screen.dart
@@ -1,12 +1,22 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
 
 class MapScreen extends StatelessWidget {
-  const MapScreen({ Key? key }) : super(key: key);
+  MapScreen({Key? key}) : super(key: key);
+
+  Completer<NaverMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("지도!")),
-    );
+        child: NaverMap(
+      onMapCreated: _onMapCreated,
+    ));
+  }
+
+  void _onMapCreated(NaverMapController controller) {
+    if (_controller.isCompleted) _controller = Completer();
+    _controller.complete(controller);
   }
 }
